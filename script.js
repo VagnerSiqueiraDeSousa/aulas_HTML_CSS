c7f7ecb6544ea8aed8b73c0478eaf6c6bf206051
VagnerSousa <107362281+VagnerSiqueiraDeSousa@users.noreply.github.com>
2022-06-12 15:40:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// while enqunto 
+
+//var letraCerta = prompt("digite a letra a ")
+//while(letraCerta !== "a"){
+  //  letraCerta = prompt("Por favor digite a letra 'a'")
+//}
+
+//alert("obrigado! ")
+
+//var total = 0
+
+//do
+
+//while ( total <=5 ){
+  //  console.log(total)
+    //total ++
+//}
+
+//console.log("saiu do while " + tatol)
+
+// agora vamos ver um pouco de for
+//for e um contator 
+//for(var i =0; i < 10; i ++){
+  // console.log(i)
+//}
+
+var listaNumeros = [1, 2, 3, 4, 5]
+ //exemplo de array formaçao de lista
+//listaNumeros[0] // 1
+//listaNumeros[1] //2
+
+for (var i =0; i< listaNumeros.length; i ++){
+    if(listaNumeros[i] ===3){
+        console.log(" numero encontrado")
+    }
+}
